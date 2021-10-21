8a8c6f2817771588720f573d6c302f6a32778882
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-21 02:30:19 -0300

    feat: adicionado uma animação ao aparecimento da smensagens

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Text, View } from "react-native";
 import { UserPhoto } from "../UserPhoto";
-
+import { MotiView } from "moti";
 import { styles } from "./styles";
 
 export type MessageProps = {
@@ -19,7 +19,12 @@ type Props = {
 };
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: "timing", duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>{data.text}</Text>
 
       <View style={styles.footer}>
@@ -31,6 +36,6 @@ export function Message({ data }: Props) {
           {data.user.name}
         </Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
