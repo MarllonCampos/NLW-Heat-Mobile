66bb5c9a344553d6aaf01f19cd8c0f8a9113d960
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-21 03:27:06 -0300

    feat: adicionado a função de logar no signInBox

--- src/components/SignInBox/index.tsx
+++ src/components/SignInBox/index.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import { View } from "react-native";
 import { COLORS } from "../../theme";
 import { Button } from "../Button";
-
+import { useAuth } from "../../hooks/auth";
 import { styles } from "./styles";
 
 export function SignInBox() {
+  const { signIn } = useAuth();
   return (
     <View style={styles.container}>
       <Button
@@ -14,6 +15,7 @@ export function SignInBox() {
         color={COLORS.BLACK_PRIMARY}
         backgroundColor={COLORS.YELLOW}
         icon="github"
+        onPress={signIn}
       />
     </View>
   );
