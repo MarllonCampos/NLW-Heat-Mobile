6933eb97af640be1cbd924f979e03c5b14b8cd3a
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-21 01:45:07 -0300

    feat: criado o compoennte de userPhoto e seus estilos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,17 +3,19 @@ import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
-
+import { UserPhoto } from "../UserPhoto";
 import LogoSvg from "../../assets/logo.svg";
 
 export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
-
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+        <UserPhoto imageUri="http://github.com/marlloncampos.png" />
+      </View>
     </View>
   );
 }
